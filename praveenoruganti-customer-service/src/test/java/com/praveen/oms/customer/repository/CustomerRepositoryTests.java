3f07c7739e8f78377e74712951945d8891dcc91c
praveen <praveen@praveen-PC>
2019-11-23 21:56:50 +0530

    updated

--- praveenoruganti-customer-service/src/test/java/com/praveen/oms/customer/repository/CustomerRepositoryTests.java
+++ praveenoruganti-customer-service/src/test/java/com/praveen/oms/customer/repository/CustomerRepositoryTests.java
@@ -3,6 +3,9 @@ package com.praveen.oms.customer.repository;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +27,35 @@ public class CustomerRepositoryTests {
 	public void testCreateCustomer() {
 		Customer customer = Customer.builder().firstname("Praveen").lastname("Oruganti")
 				.email("praveenoruganti@gmail.com").creationdate("20/11/2019").build();
-		customerRepository.saveAndFlush(customer);
-		Customer custome2 = customerRepository.findAll().get(0);
+		Customer custome2 =customerRepository.saveAndFlush(customer);			
 		assertNotNull(customer);
 		assertEquals(custome2.getFirstname(), customer.getFirstname());
 		assertEquals(custome2.getLastname(), customer.getLastname());
+	}
+	
+	
+	@Test
+	public void testGetCustomer() {
+		List<Customer> listOfExpectedCustomers = new ArrayList<Customer>();
+		Customer custOne = Customer.builder().firstname("Praveen").lastname("Oruganti")
+				.email("praveenoruganti@gmail.com").creationdate("20/11/2019").build();
+		Customer custTwo = Customer.builder().firstname("Kiran").lastname("Oruganti").email("kiranbadugu@yahoo.com")
+				.creationdate("21/11/2019").build();
+		Customer custThree = Customer.builder().firstname("Praneeth").lastname("Vishnubhotla")
+				.email("praneethv@gmail.com").creationdate("22/11/2019").build();
+		
+		listOfExpectedCustomers.add(custOne);
+		listOfExpectedCustomers.add(custTwo);
+		listOfExpectedCustomers.add(custThree);
+		
+		customerRepository.saveAll(listOfExpectedCustomers);
+		
+		List<Customer> listOfActualCustomers = customerRepository.findAll();
+				
+		assertNotNull(listOfActualCustomers);
+		
+		assertEquals(listOfExpectedCustomers.size(), listOfActualCustomers.size());
+	
 	}
 
 }
