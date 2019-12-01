6636e3b47860ac42e9f7c80e02cd757cd86a7664
praveen <praveen@praveen-PC>
2019-12-01 17:51:18 +0530

    updated

--- praveenoruganti-eureka-server/src/main/java/com/praveen/eureka/server/EurekaServerApplication.java
+++ praveenoruganti-eureka-server/src/main/java/com/praveen/eureka/server/EurekaServerApplication.java
@@ -1,8 +1,17 @@
 package com.praveen.eureka.server;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,6 +19,18 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
+
+@RestController
+class ServiceInstanceRestController {
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
 	}
 
 }
