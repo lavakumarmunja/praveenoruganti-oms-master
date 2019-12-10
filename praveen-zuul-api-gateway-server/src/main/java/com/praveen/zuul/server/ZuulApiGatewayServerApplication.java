c2da57d5ca850d71cb240b68d2d3eeb1d33e5097
praveen <praveen@praveen-PC>
2019-12-10 12:01:40 +0530

    updated

---
+++ praveen-zuul-api-gateway-server/src/main/java/com/praveen/zuul/server/ZuulApiGatewayServerApplication.java
@@ -0,0 +1,40 @@
+package com.praveen.zuul.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.praveen.zuul.server.filters.ErrorFilter;
+import com.praveen.zuul.server.filters.PostFilter;
+import com.praveen.zuul.server.filters.PreFilter;
+import com.praveen.zuul.server.filters.RouteFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayServerApplication.class, args);
+	}
+	
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+
+}
---
+++ praveen-zuul-api-gateway-server/src/test/java/com/praveen/zuul/server/ZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.zuul.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
